cf5088e75ee244623282a13a2ccb2756f96b6dfb
wunderlicht <wunderlich@actualchange.eu>
2023-06-17 17:28:41 +0200

    Add initial rtf generation

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/wunderlicht/einmaleins
 
 go 1.20
+
+require github.com/therox/rtf-doc v0.8.27
--- main.go
+++ main.go
@@ -2,16 +2,37 @@ package main
 
 import (
 	"fmt"
+	rtfdoc "github.com/therox/rtf-doc"
 	"math/rand"
+	"os"
 )
 
 const (
 	upperBound   = 10
 	linesPerPage = 20
+	color        = rtfdoc.ColorBlack
+	font         = rtfdoc.FontTimesNewRoman
+	fontSize     = 20
 )
 
 func main() {
+	//setup the document
+	doc := rtfdoc.NewDocument()
+	doc.SetFormat(rtfdoc.FormatA4)
+	doc.SetOrientation(rtfdoc.OrientationPortrait)
+
+	//add tasks to the document
 	for i := 0; i < linesPerPage; i++ {
-		fmt.Printf("%d * %d = \n", rand.Intn(upperBound+1), rand.Intn(upperBound+1))
+		taskText := fmt.Sprintf("%d * %d =", rand.Intn(upperBound+1), rand.Intn(upperBound+1))
+		taskParagraph := doc.AddParagraph()
+		taskParagraph.SetAlign(rtfdoc.AlignLeft)
+		taskParagraph.AddText(taskText, fontSize, font, color)
+		taskParagraph.AddNewLine()
+	}
+
+	//write it out to a file
+	err := os.WriteFile("einmaleins.rtf", doc.Export(), 0644)
+	if err != nil {
+		panic(err)
 	}
 }
